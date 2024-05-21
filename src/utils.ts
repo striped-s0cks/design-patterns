8bb690a0fbce172ce42b11353f2faf509578242d
striped_s0cks <yanka.savchenko@gmail.com>
2024-05-20 17:37:30 -0700

    add utils

---
+++ src/utils.ts
@@ -0,0 +1,36 @@
+/**
+ * Returns the element with id "root".
+ */
+export function getRoot(): HTMLElement {
+    return document.getElementById('root');
+}
+
+/**
+ * Adds an h1 element with the provided text to the root element.
+ */
+export function appendHeader(text: string): void {
+    const header = document.createElement('h1');
+    header.append(text);
+    getRoot().append(header);
+}
+
+/**
+ * Adds a div with provided logs to the root element.
+ */
+export function appendLog(log: string): void {
+    const li = document.createElement('li');
+    li.append(log);
+    getRoot().append(li);
+}
+
+/**
+ * Adds a divider to the root element.
+ */
+export function appendDivider(): void {
+    getRoot().append('--------------------------------------------------------------------------------');
+}
+
+export function appendBreak(): void {
+    const br = document.createElement('br');
+    getRoot().append(br);
+}
