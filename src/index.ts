2b758904de52067e6df3a416f9e68873f6dbabe6
striped_s0cks <yanka.savchenko@gmail.com>
2024-05-20 17:38:45 -0700

    add duck simulator

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
-const div: HTMLElement = document.getElementById('root');
-div.innerHTML = `HELLO WoRLD`;
+import MallardDuck from "./simUDuck/ducks/mallardDuck";
+import ModelDuck from "./simUDuck/ducks/modelDuck";
+import FlyRocketPowered from "./simUDuck/flyBehaviors/flyRocketPowered";
+import { appendBreak, appendDivider, appendHeader, appendLog } from "./utils";
+
+appendHeader('Duck Simulator');
+
+const mallard = new MallardDuck();
+mallard.display();
+mallard.performQuack();
+mallard.performFly();
+
+appendBreak();
+
+const model = new ModelDuck();
+model.display();
+model.performFly();
+model.setFlyBehavior(new FlyRocketPowered());
+model.performFly();
+
+appendDivider();
