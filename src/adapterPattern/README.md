1146865b611cd4acc2f383d97e1880825910174e
striped_s0cks <yanka.savchenko@gmail.com>
2024-05-31 10:56:15 -0700

    add adapter pattern

---
+++ src/adapterPattern/README.md
@@ -0,0 +1,2 @@
+*The Adapter Pattern* converts the interface of a class into another interface the client expects. Adapter lets classes
+work together that couldn't otherwise because of incompatible interfaces.
---
+++ src/adapterPattern/index.ts
@@ -0,0 +1,37 @@
+import { HeaderType, appendBreak, appendDivider, appendHeader, appendLog } from "../utils";
+import DuckAdapter from "./adapters/duckAdapter";
+import TurkeyAdapter from "./adapters/turkeyAdapter";
+import MallardDuck from "./birds/mallardDuck";
+import WildTurkey from "./birds/wildTurkey";
+
+appendHeader('Adapter Pattern', HeaderType.h1);
+appendHeader('Turkey Adapter', HeaderType.h2);
+
+const duck = new MallardDuck();
+const turkey = new WildTurkey();
+const turkeyAdapter = new TurkeyAdapter(turkey);
+const duckAdapter = new DuckAdapter(duck);
+
+appendLog('Turkey says:');
+turkey.gobble();
+turkey.fly();
+
+appendBreak();
+
+appendLog('Duck says:');
+duck.quack();
+duck.fly();
+
+appendBreak();
+
+appendLog('TurkeyAdapter says:');
+turkeyAdapter.quack();
+turkeyAdapter.fly();
+
+appendBreak();
+
+appendLog('DuckAdapter says:');
+duckAdapter.gobble();
+duckAdapter.fly();
+
+appendDivider();
